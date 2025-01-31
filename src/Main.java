f9f023ca1624b3cf4d91052379cf21aa05538b7e
Michael <zerhusma@mail.uc.edu>
2025-01-31 16:04:34 -0500

    Completed commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double creditBalance = 5000.00;
+        final double INTEREST_RATE = .17;
+        double monthlyInterest = 0;
+        int monthsElapsed = 0;
+
+        monthlyInterest = creditBalance * INTEREST_RATE;
+        monthlyInterest = monthlyInterest * 100; //used to get rid of floating decimal
+        monthlyInterest = Math.round(monthlyInterest);
+        monthlyInterest = monthlyInterest / 100;
+        creditBalance = creditBalance + monthlyInterest;
+        monthsElapsed++;
+        System.out.println("The monthly interest for month " + monthsElapsed + " is " + monthlyInterest + " bringing your balance to " + creditBalance + ".");
+        monthlyInterest = creditBalance * INTEREST_RATE;
+        monthlyInterest = monthlyInterest * 100; //used to get rid of floating decimal
+        monthlyInterest = Math.round(monthlyInterest);
+        monthlyInterest = monthlyInterest / 100;
+        creditBalance = creditBalance + monthlyInterest;
+        monthsElapsed++;
+        System.out.println("The monthly interest for month " + monthsElapsed + " is " + monthlyInterest + " bringing your balance to " + creditBalance + ".");
+
+    }
+}
